2d64b0baa3e5520406e7ab09af0f891b2492ee1e
Laura Felce <crepu83@yahoo.it>
2019-03-20 15:46:12 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,15 +3,15 @@ class PigLatinizer
 
 
   def piglatinize(user_phrase)
-    if (user_phrase.split(" ").length == 1
+    if user_phrase.split(" ").length == 1
       piglatinize_word(user_phrase)
     else
-      piglatinize_sentence
+      piglatinize_sentence(user_phrase)
     end
   end
 
 
-
+  
 
 
     phrase = words.split(" ")
