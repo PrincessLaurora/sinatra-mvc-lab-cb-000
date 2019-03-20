dcb43eb81fd3fefab7acb2295dd5ec01790cb1c7
Laura Felce <crepu83@yahoo.it>
2019-03-20 15:52:20 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,17 +10,25 @@ class PigLatinizer
     end
   end
 
-
-
-
-
-    phrase = words.split(" ")
-    phrase.each do |word|
-      if word.start_with?('a','e','i','o','u')
-        word << "ay"
-      end
-      word
+  def consonant?(letter)
+      !letter.match(/[aAeEiIoOuU]/)
     end
-  end
+
+  def piglatinize_word(word)
+      if !consonant?(word[0])
+        word = word + "w"
+      elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+        word = word.slice(3..-1) + word.slice(0,3)
+      elsif consonant?(word[0]) && consonant?(word[1])
+        word = word.slice(2..-1) + word.slice(0,2)
+      else
+        word = word.slice(1..-1) + word.slice(0)
+      end
+      word << "ay"
+    end
+
+    def piglatinize_sentence(sentence)
+      sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+    end
 
 end
